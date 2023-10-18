4743e4f7242f8bd5f1f7b211f3333eae15972ee7
Eclipsee-18 <147718788+GameDevVaibhav@users.noreply.github.com>
2023-10-18 18:25:34 +0530

    ChangeAnimation done

--- Basic_2d_game/Assets/Scripts/Player.cs
+++ Basic_2d_game/Assets/Scripts/Player.cs
@@ -7,6 +7,8 @@ public class Player : MonoBehaviour
 {
     
     Rigidbody2D myRigidbody2D;
+    Animator myAnimator;
+
     [SerializeField]
     float speed = 2f;
     // Start is called before the first frame update
@@ -14,6 +16,7 @@ public class Player : MonoBehaviour
     {
         
         myRigidbody2D= GetComponent<Rigidbody2D>();
+        myAnimator= GetComponent<Animator>();
         
     }
 
@@ -31,6 +34,10 @@ public class Player : MonoBehaviour
 
         // Apply the input to the player's velocity.
         myRigidbody2D.velocity = new Vector2(horizontalInput * speed, myRigidbody2D.velocity.y);
+        FlipSprite();
+        ChangeRunAnimation();
+        ChangeJumpAnimation();
+       
     }
 
     public void Jump(InputAction.CallbackContext context)
@@ -40,5 +47,26 @@ public class Player : MonoBehaviour
             Debug.Log("Jump!" + context.phase);
             myRigidbody2D.AddForce(Vector2.up * speed, ForceMode2D.Impulse);
         }
-    }   
+    }
+
+    private void ChangeRunAnimation()
+    {
+        bool runningHorizontally = Mathf.Abs(myRigidbody2D.velocity.x) > Mathf.Epsilon;
+        myAnimator.SetBool("Running", runningHorizontally);
+    }
+
+    private void ChangeJumpAnimation()
+    {
+        bool jumping=Mathf.Abs(myRigidbody2D.velocity.y) > Mathf.Epsilon;
+        myAnimator.SetBool("Climbing", jumping);
+    }
+
+    private void FlipSprite()
+    {
+        bool runningHorizontally = Mathf.Abs(myRigidbody2D.velocity.x) > Mathf.Epsilon;
+        if (runningHorizontally)
+        {
+            transform.localScale = new Vector2(Mathf.Sign(myRigidbody2D.velocity.x),1f);
+        }
+    }
 }
