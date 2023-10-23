c3781492169c81961e3857447773bd248bb57df9
GameDevVaibhav <147718788+GameDevVaibhav@users.noreply.github.com>
2023-10-23 13:41:26 +0530

    Explosion Impact added

--- Basic_2d_game/Assets/Scripts/Bomb.cs
+++ Basic_2d_game/Assets/Scripts/Bomb.cs
@@ -5,18 +5,27 @@ using UnityEngine;
 public class Bomb : MonoBehaviour
 {
     Animator myAnimator;
+    [SerializeField] float radius = 3f;
+    [SerializeField] Vector2 explosionForce=new Vector2(200f,200f);
     // Start is called before the first frame update
     void Start()
     {
         myAnimator= GetComponent<Animator>();
     }
 
-    // Update is called once per frame
-    void Update()
+    void ExplodeBomb()
     {
-        
+        Collider2D playerCollider = Physics2D.OverlapCircle(transform.position, radius, LayerMask.GetMask("Player"));
+        if (playerCollider)
+        {
+            playerCollider.GetComponent<Rigidbody2D>().AddForce(explosionForce);
+            playerCollider.GetComponent<Player>().PlayerHit();
+        }
     }
 
+   
+
+  
     private void OnTriggerEnter2D(Collider2D collision)
     {
         myAnimator.SetTrigger("Burn");
@@ -25,5 +34,10 @@ public class Bomb : MonoBehaviour
     void DestroyBomb()
     {
         Destroy(gameObject);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawWireSphere(transform.position, radius);
     }
 }
