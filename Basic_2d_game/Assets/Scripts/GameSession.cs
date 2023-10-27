e9983bdcf9a698443523fec7ab316acec2c78850
GameDevVaibhav <147718788+GameDevVaibhav@users.noreply.github.com>
2023-10-27 12:44:41 +0530

    Added Heart bar

--- Basic_2d_game/Assets/Scripts/GameSession.cs
+++ Basic_2d_game/Assets/Scripts/GameSession.cs
@@ -13,6 +13,8 @@ public class GameSession : MonoBehaviour
     [SerializeField] int playerLives = 3, score=0;
 
     [SerializeField] TextMeshProUGUI scoreText, livesText;
+
+    [SerializeField] Image[] hearts;
     private void Awake()
     {
         int numGameSessions=FindObjectsOfType<GameSession>().Length;
@@ -41,7 +43,14 @@ public class GameSession : MonoBehaviour
 
     public void AddToLives() 
     {
+
         playerLives += 1;
+
+        if(playerLives>= 3) 
+        {
+            playerLives = 3;
+        }
+        UpdateHearts();
         livesText.text = playerLives.ToString();
     }
 
@@ -66,7 +75,23 @@ public class GameSession : MonoBehaviour
     private void TakeLife()
     {
         playerLives--;
+        UpdateHearts();
         livesText.text = playerLives.ToString();
 
+    }
+
+    private void UpdateHearts()
+    {
+        for(int i=0;i<hearts.Length;i++)
+        {
+            if (i < playerLives)
+            {
+                hearts[i].enabled = true;
+            }
+            else
+            {
+                hearts[i].enabled = false;
+            }
+        }
     }
 }
